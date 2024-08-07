332722065be2218c0d86cc94993e1d93fd940444
unknown <lzdavi1325@gmail.com>
2024-08-07 17:00:58 -0300

    initial setup

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/courses", (request, response) => {
+	const { name } = request.body;
+
+	return response.json({ name });
+});
+
+app.listen(3000, () => {
+	console.log("Server is running!");
+});
